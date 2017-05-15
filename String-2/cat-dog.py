71b2cc3025a614315eea4d7766ee8dbe94d989a9
Abdulrahman Alfawzan <mhagr500@gmail.com>
2017-05-15 16:49:32 -0500

    Create cat-dog.py

---
+++ String-2/cat-dog.py
@@ -0,0 +1,18 @@
+
+#cat-dog
+
+
+"""
+Return True if the string "cat" and "dog" appear the same number of times in the given string.
+
+cat_dog('catdog') → True
+cat_dog('catcat') → False
+cat_dog('1cat1cadodog') → True
+
+"""
+def cat_dog(str):
+  c = str.count("cat")
+  d = str.count("dog")
+  if c == d:
+    return True
+  return False
